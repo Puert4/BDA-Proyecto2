cb25a673366fdaabbbeb7e4bc36427066c35fdf4
Puert4 <jullian.puerta248239@potros.itson.edu.mx>
2024-03-21 10:15:28 -0700

    GUI
    Agregue las interfazes graficas GUI, aun falta agregar lo de fecha, ver si quieren usar un calendar, etc.

---
+++ AgenciaTributariaPresentacion/src/main/java/com/mycompany/agenciatributariapresentacion/AgenciaTributariaPresentacion.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agenciatributariapresentacion;
+
+/**
+ *
+ * @author TeLesheo
+ */
+public class AgenciaTributariaPresentacion {
+
+    public static void main(String[] args) {
+    }
+}
