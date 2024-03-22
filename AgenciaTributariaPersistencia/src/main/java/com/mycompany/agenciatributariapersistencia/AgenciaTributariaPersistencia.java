7d9935646556954cabad5a0c45089f88c392fb67
Puert4 <jullian.puerta248239@potros.itson.edu.mx>
2024-03-22 16:29:12 -0700

    AgenciaTributariaPersistencia
    Se agrego el proyecto AgenciaTributariaNegocio

---
+++ AgenciaTributariaPersistencia/src/main/java/com/mycompany/agenciatributariapersistencia/AgenciaTributariaPersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agenciatributariapersistencia;
+
+/**
+ *
+ * @author TeLesheo
+ */
+public class AgenciaTributariaPersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
