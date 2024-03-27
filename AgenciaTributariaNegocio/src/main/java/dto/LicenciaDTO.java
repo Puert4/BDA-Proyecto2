e57c9814d35e3b1960d86fed8b048738103b419f
Puert4 <jullian.puerta248239@potros.itson.edu.mx>
2024-03-27 10:02:51 -0700

    Negocio
    Se agrego DTO

--- AgenciaTributariaNegocio/src/main/java/dto/LicenciaDTO.java
+++ AgenciaTributariaNegocio/src/main/java/dto/LicenciaDTO.java
@@ -7,7 +7,42 @@ import java.util.Calendar;
  * @author TeLesheo
  */
 public class LicenciaDTO {
+
     private Double costo;
     private Calendar vigencia;
     private Calendar fechaEmision;
+
+    public LicenciaDTO() {
+    }
+
+    public LicenciaDTO(Double costo, Calendar vigencia, Calendar fechaEmision) {
+        this.costo = costo;
+        this.vigencia = vigencia;
+        this.fechaEmision = fechaEmision;
+    }
+
+    public Double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(Double costo) {
+        this.costo = costo;
+    }
+
+    public Calendar getVigencia() {
+        return vigencia;
+    }
+
+    public void setVigencia(Calendar vigencia) {
+        this.vigencia = vigencia;
+    }
+
+    public Calendar getFechaEmision() {
+        return fechaEmision;
+    }
+
+    public void setFechaEmision(Calendar fechaEmision) {
+        this.fechaEmision = fechaEmision;
+    }
+
 }
